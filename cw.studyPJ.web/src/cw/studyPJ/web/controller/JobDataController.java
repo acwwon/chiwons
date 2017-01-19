83182b295fe38933bdb1f234c087d4c9e59c6893
acwwon <acwwon@naver.com>
2017-01-19 16:00:17 +0900

    today second commit

--- cw.studyPJ.web/src/cw/studyPJ/web/controller/JobDataController.java
+++ cw.studyPJ.web/src/cw/studyPJ/web/controller/JobDataController.java
@@ -1,19 +1,14 @@
 package cw.studyPJ.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import cw.studyPJ.domain.JobData;
-import cw.studyPJ.service.JobDataService;
 
 @Controller
 @RequestMapping("/jobdata")
 public class JobDataController {
 	
-	@Autowired
-	JobDataService jobDataService; 
 	
 	@RequestMapping("/main.do")
 	public ModelAndView mainViewer(){
@@ -21,9 +16,7 @@ public class JobDataController {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName("/main/main.jsp");
 		
-		JobData jobData = jobDataService.retrieveByJobDataId("nextree");
-		
-		mav.addObject("jobData", jobData);
+
 		
 		return mav;
 	
